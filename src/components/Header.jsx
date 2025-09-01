185e2fb0b3e27c74536d8686d51ba119f31cb891
KabirHas <kabir.infoseek@gmail.com>
2025-09-01 13:17:46 +0530

    user-sidebar changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../asset/css/Header.css';
 import { useNavigate } from "react-router-dom";
 
 const Header = () => {
   const navigate = useNavigate();
   const [menuOpen, setMenuOpen] = useState(false);
+  const [hasToken, setHasToken] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token"); // or sessionStorage
+    if (token) {
+      setHasToken(true);
+    }
+  }, []);
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
@@ -30,8 +38,14 @@ const Header = () => {
         <a href="#Find-Hostess" onClick={closeMenu}>Find Hostess</a>
         <a href="#FAQs" onClick={closeMenu}>FAQs</a>
         <a href="#How-It-Works" onClick={closeMenu}>How It Works</a>
-        <button className="btns login" onClick={() => { closeMenu(); navigate("/login"); }}>Log In</button>
-        <button className="btns signup" onClick={() => { closeMenu(); navigate("/register"); }}>Sign Up</button>
+        {hasToken ? (
+          <button className="btns dashboard signup" onClick={() => { closeMenu(); navigate("/dashboard"); }}>Dashboard</button>
+        ) : (
+          <>
+            <button className="btns login" onClick={() => { closeMenu(); navigate("/login"); }}>Log In</button>
+            <button className="btns signup" onClick={() => { closeMenu(); navigate("/register"); }}>Sign Up</button>
+          </>
+        )}
       </nav>
     </header>
   );
